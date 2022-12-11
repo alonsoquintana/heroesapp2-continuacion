6857ca07e6a448113d2593d2acf0bd4396388dc8
Alonso Paucar <iaquintanapa@gmail.com>
2022-12-10 22:13:21 -0500

    Continuando desde la tercera parte del proyecto

---
+++ src/app/heroes/services/heroes.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { Heroe } from '../interfaces/heroes.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroesService {
+
+  constructor( private http: HttpClient ) { }
+
+  getHeroes(): Observable<Heroe[]> {
+    return this.http.get<Heroe[]>(`http://localhost:3000/heroes`);
+  }
+
+  getHeroePorId( id: string ):Observable<Heroe> {
+    return this.http.get<Heroe>(`http://localhost:3000/heroes/${ id }`);
+  }
+
+  getSugerencias( termino: string ): Observable<Heroe[]> {
+    return this.http.get<Heroe[]>(`http://localhost:3000/heroes?q=${ termino }&_limit=6`);
+  }
+
+  agregarHeroe( heroe: Heroe ): Observable<Heroe> {
+    return this.http.post<Heroe>(`http://localhost:3000/heroes`, heroe );
+  }
+
+  actualizarHeroe( heroe: Heroe ): Observable<Heroe>{
+    return this.http.put<Heroe>(`http://localhost:3000/heroes/${ heroe.id }`, heroe );
+  }
+
+  borrarHeroe( id: string ): Observable<any>{
+    return this.http.delete<any>(`http://localhost:3000/heroes/${ id }`);
+  }
+
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  baseUrl: 'http://anakin-skywalker.com/api'
+
+};
