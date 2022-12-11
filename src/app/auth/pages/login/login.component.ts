a91efebaf6086f3b8baaaf9e76d26101c74fa8cc
Alonso Paucar <iaquintanapa@gmail.com>
2022-12-11 08:14:46 -0500

    Angular Guards - Canload

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -9,15 +10,26 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent {
 
-  constructor( private router: Router) { }
+  constructor( private router: Router,
+               private authservice:AuthService ) { }
 
   login() {
 
     // Ir al backend
     // un usuario
+    this.authservice.login()
+      .subscribe( resp => {
+        console.log(resp);
 
+        if( resp.id ) {
+          this.router.navigate(['./heroes'])
+        }
+      })
+  }
+
+  ingresarSinLogin() {
+    this.authservice.logout();
     this.router.navigate(['./heroes'])
-
   }
 
 }
